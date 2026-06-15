d4620587ee325f472dcf20e42b47148158c61506
yglee <yglee0248@gmail.com>
2026-06-15 17:43:41 +0900

    chore: reset generated backend setup

--- backend/src/main/java/com/subtrack/global/config/MyBatisConfig.java
+++
@@ -1,9 +0,0 @@
-package com.subtrack.global.config;
-
-import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@MapperScan(basePackages = "com.subtrack.domain")
-public class MyBatisConfig {
-}
